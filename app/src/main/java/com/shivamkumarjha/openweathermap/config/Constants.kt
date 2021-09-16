e7390e8f9bdde202a1336023f4d768c01134fd72
ShivamKumarJha <jha.shivam3@gmail.com>
2021-09-17 00:27:30 +0530

    Add config
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ app/src/main/java/com/shivamkumarjha/openweathermap/config/Constants.kt
@@ -0,0 +1,12 @@
+package com.shivamkumarjha.openweathermap.config
+
+import com.shivamkumarjha.openweathermap.BuildConfig
+
+object Constants {
+    const val TAG = "OpenWeatherMap"
+
+    // API
+    const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
+    const val DEFAULT_LOCATION = "Bengaluru"
+    const val APP_ID = BuildConfig.APP_ID
+}
